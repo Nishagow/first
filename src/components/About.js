7be10974bf70731e24046bb44eea4277c72599ba
Nishagow <nishanithin2607@gmail.com>
2024-05-31 12:11:40 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css'
+
+function About() {
+  return (
+    <div>
+      <h1 style={{fontSize:'40px',color:'red'}}>About Us</h1>
+      <h2>24/7 service available</h2>
+      <h2>Contact us:08176-658555</h2>
+      <h2>Email :anaga@gmail.com</h2>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+  return (
+    <center>
+    <div>
+      <h1 style={{fontSize:'50px',color:'white'}}>Welcome to our website</h1>
+      <p style={{fontSize:'120px', color:'white'}}>We belive we can all make different</p>
+    </div>
+    </center>
+
+  );
+}
+
+export default Home;
